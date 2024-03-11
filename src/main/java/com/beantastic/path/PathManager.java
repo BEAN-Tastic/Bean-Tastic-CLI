04114e5d72f741601e081fa4eb83c913d1721413
Liam Quick <liam.quick@vcontractor.co.za>
2024-03-11 11:23:45 +0200

    fixing PathManager warnings

--- src/main/java/com/beantastic/path/PathManager.java
+++ src/main/java/com/beantastic/path/PathManager.java
@@ -1,8 +1,5 @@
 package com.beantastic.path;
 
-import static java.lang.StringTemplate.STR;
-
-import java.security.PublicKey;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,9 +10,9 @@ import com.beantastic.player.PlayerManager;
 
 public class PathManager {
     private static int levelDifficulty = 1;
-    private static int numOfStagesInPath = 3;
+    private static final int numOfStagesInPath = 3;
 
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     public static void resetLevelDifficulty(){
         levelDifficulty = 1;
@@ -23,12 +20,19 @@ public class PathManager {
 
     //START PATH\\
     public static void startPath(Scanner scanner){
-        Main.typewriter("You wake up finding yourself...\nOn the kitchen floor... No bean has ever ventured this low before, well not any self-respecting bean that is... The kitchen counter looms above you, your home.\n\n"
-            + "Down on the floor, it's a different story. The floor is a realm of forgotten leftovers, crumbs, and the occasional sticky spill. It's where the unwanted and unappreciated foods reside, far from the glory of the counter above. As a bean, you've always been taught to stay on the counter, to avoid the disgrace of the floor at all costs."
-            + "\n\nYet, here you are, having fallen off the counter in a moment of carelessness or perhaps curiosity. The floor stretches out before you, mysterious and unfamiliar. The journey back to the counter seems daunting, but staying on the floor is not an option. What will you do?");
+        Main.typewriter("""
+                You wake up finding yourself...
+                On the kitchen floor... No bean has ever ventured this low before, well not any self-respecting bean that is... The kitchen counter looms above you, your home.
 
-        Main.typewriter("\n1. Find a way back home \n" + 
-            "\n2. Panic!");
+                Down on the floor, it's a different story. The floor is a realm of forgotten leftovers, crumbs, and the occasional sticky spill. It's where the unwanted and unappreciated foods reside, far from the glory of the counter above. As a bean, you've always been taught to stay on the counter, to avoid the disgrace of the floor at all costs.
+
+                Yet, here you are, having fallen off the counter in a moment of carelessness or perhaps curiosity. The floor stretches out before you, mysterious and unfamiliar. The journey back to the counter seems daunting, but staying on the floor is not an option. What will you do?""");
+
+        Main.typewriter("""
+
+                1. Find a way back home\s
+
+                2. Panic!""");
 
         pickPath(scanner);
     }
@@ -36,11 +40,14 @@ public class PathManager {
     public static void pickPath(Scanner scanner){
         String pathOption = scanner.nextLine().toLowerCase();
 
-        if(pathOption.equals("1") || pathOption.equals("one") || pathOption.equals("Find a way back home")){
+        if(pathOption.equals("1") || pathOption.equals("one") || pathOption.equals("find a way back home")){
 
-            Main.typewriter("\nYou move forward, looking for a way out" + 
-                "\n1. Keep walking forward\n" + 
-                "\n2. Look around");
+            Main.typewriter("""
+
+                    You move forward, looking for a way out
+                    1. Keep walking forward
+
+                    2. Look around""");
 
             keepWalking(scanner);
 
@@ -53,48 +60,63 @@ public class PathManager {
     }
 
     public static void panic(Scanner scanner){
-        Main.typewriter("\nYou start to panic...\n" + 
-            "\n1. Curl into a ball and hope for the best.\n" + 
-            "\n2. Fall to the floor and sob.\n" + 
-            "\n3. Panic more!\n");
+        Main.typewriter("""
+
+                You start to panic...
+
+                1. Curl into a ball and hope for the best.
+
+                2. Fall to the floor and sob.
+
+                3. Panic more!
+                """);
 
         panicOptions(scanner);
-        
+
     }
 
     public static void panicOptions(Scanner scanner){
         String pathOption = scanner.nextLine().toLowerCase();
 
-        if(pathOption.equals("1") || pathOption.equals("one") || pathOption.equals("curl into a ball")){
+        switch (pathOption) {
+            case "1", "one", "curl into a ball" -> {
+            }
+            case "2", "two", "sob" -> {
 
-        } else if(pathOption.equals("2") || pathOption.equals("two") || pathOption.equals("sob")){
+                Main.typewriter("""
+                        You make so much noise crying you attract something...
+                        You hear it getting closer...\s
+                        And closer...
+                        """);
+                CombatSystem.startCombatEvent(scanner, 1);
+            }
+            case "3", "three", "panic" -> {
 
-            Main.typewriter("You make so much noise crying you attract something...\n" + 
-                "You hear it getting closer... \nAnd closer..." + "\n");
-            CombatSystem.startCombatEvent(scanner, 1);
+                Main.typewriter("You panic so much you explode!");
+                PlayerManager.getPlayer().die();
+            }
+            default -> {
 
-        } else if(pathOption.equals("3") || pathOption.equals("three") || pathOption.equals("panic")){
-
-            Main.typewriter("You panic so much you explode!");
-            PlayerManager.getPlayer().die();
-
-        }else{
-
-            Main.typewriter("\nPlease input a valid option");
-            panicOptions(scanner);
+                Main.typewriter("\nPlease input a valid option");
+                panicOptions(scanner);
+            }
         }
     }
 
     public static void keepWalking(Scanner scanner){
         String pathOption = scanner.nextLine().toLowerCase();
 
-        if(pathOption.equals("1") || pathOption.equals("one") || pathOption.equals("keep walking forward")){
-            //add some dialogue here
-        } else if(pathOption.equals("2") || pathOption.equals("two") || pathOption.equals("look around")){
-            //add some dialogue here
-        } else{
-            Main.typewriter("Please input a valid option");
-            keepWalking(scanner);
+        switch (pathOption) {
+            case "1", "one", "keep walking forward" -> {
+                //TODO: add some dialogue here for 1st path
+            }
+            case "2", "two", "look around" -> {
+                //TODO: add some dialogue here for 2nd path
+            }
+            default -> {
+                Main.typewriter("Please input a valid option");
+                keepWalking(scanner);
+            }
         }
 
     }
@@ -106,19 +128,19 @@ public class PathManager {
             if (PlayerManager.getPlayer().isDead() || PlayerManager.getPlayer().isRizzedUp()) {
                 return;
             }
-    
+
             CombatSystem.startCombatEvent(scanner, levelDifficulty);
             levelDifficulty++;
             ObstacleSystem.obstacle(random, scanner);
         }
-    
+
         endPath();
     }
 
 
     //END PATH\\
     public static void endPath(){
-        Main.typewriter("After your long tretious journey, you finally see it... The light at the end of the tunnel" + 
+        Main.typewriter("After your long treacherous journey, you finally see it... The light at the end of the tunnel" +
             "The way up and home, away from this dark place");
         //game over
         Main.gameOver(true);
