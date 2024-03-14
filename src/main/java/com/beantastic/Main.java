20dfaec990280331d121087c22d472e8b580fb0a
Liam Talberg <liamtalberg@gmail.com>
2024-03-14 16:32:59 +0200

    Added oauth

--- src/main/java/com/beantastic/Main.java
+++ src/main/java/com/beantastic/Main.java
@@ -1,5 +1,6 @@
 package com.beantastic;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -13,6 +14,7 @@ import com.beantastic.items.ItemManager;
 import com.beantastic.logging.ChoiceOption;
 import com.beantastic.logging.Logger;
 import com.beantastic.logging.UserChoice;
+import com.beantastic.oauth.GitHubOAuth;
 import com.beantastic.path.PathManager;
 import com.beantastic.player.Player;
 import com.beantastic.player.PlayerClass;
@@ -22,139 +24,146 @@ import com.beantastic.stats.StatBlock;
 
 public class Main {
 
-    private final Random random;
+        private final Random random;
 
-    private final Logger logger;
-    private final Scanner scanner;
-    private final EnemyManager enemyManager;
-    private final ItemManager itemManager;
-    private final PlayerManager playerManager;
+        private final Logger logger;
+        private final Scanner scanner;
+        private final EnemyManager enemyManager;
+        private final ItemManager itemManager;
+        private final PlayerManager playerManager;
 
-    public Main(Random random, Logger logger, Scanner scanner, EnemyManager enemyManager, ItemManager itemManager, PlayerManager playerManager) {
-        this.random = random;
-        this.logger = logger;
-        this.scanner = scanner;
-        this.enemyManager = enemyManager;
-        this.itemManager = itemManager;
-        this.playerManager = playerManager;
-    }
-
-
-    public static void main(String[] args) {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        Logger logger = new Logger(System.out);
-
-        List<PlayerClass> playerClasses = ApitController.getClasses();
-        PlayerManager playerManager = new PlayerManager(logger, scanner, playerClasses);
-
-        List<Enemy> enemies = ApitController.getEnemies();
-        EnemyManager enemyManager = new EnemyManager(random, enemies.toArray(Enemy[]::new));
-        List<ItemClass> items = ApitController.getItems();
-        ItemManager itemManager = new ItemManager(logger, random, scanner, items.toArray(ItemClass[]::new));
-        Main main = new Main(random, logger, scanner, enemyManager, itemManager, playerManager);
-
-        // START GAME
-        boolean play;
-        do {
-            play = main.startGame();
-        } while (play);
-
-    }
-
-    //TEMP DATA INSERTION\\
-    public static PlayerClassManager addClass(){
-        return new PlayerClassManager(
-                new PlayerClass("Green Bean Warrior",
-                        "A sturdy bean skilled in close combat, wielding the might of beans with brute force.",
-                        new StatBlock(5, 3, 2, 2)),
-                new PlayerClass("Magic Coffee Bean",
-                        "A magical bean adept at harnessing the mystical energies of beans to cast powerful spells.",
-                        new StatBlock(3, 2, 3, 3)),
-                new PlayerClass("Sneaky Black Bean",
-                        "A nimble bean with a knack for stealth and deception, striking enemies from the shadows.",
-                        new StatBlock(5, 3, 2, 2)),
-                new PlayerClass("Divine Lima Bean",
-                        "A benevolent bean with divine powers, healing allies and smiting foes with righteous fury.",
-                        new StatBlock(5, 3, 2, 2)),
-                new PlayerClass("Arrowhead Pinto Bean",
-                        "A skilled bean archer, raining down arrows upon enemies with deadly precision.",
-                        new StatBlock(5, 3, 2, 2)));
-    }
-
-    public static ItemManager addItems(Logger logger, Random random, Scanner scanner){
-        return new ItemManager(
-                logger,
-                random,
-                scanner,
-                new ItemClass("SHOE", "HOT PINK HIGH HEEL", new StatBlock(0, 0, 0, 1), "Common"),
-                new ItemClass("tea cup", "steaming black tea", new StatBlock(0, 0, -1, 0), "Rare"));
-    }
-
-    public static EnemyManager addEnemies(Random random){
-        return new EnemyManager(random,
-                new Enemy("Sorcerer Soup", new StatBlock(3, 0, 1, 2), "Easy",
-                        "Fiery Ladle Strike- The Sorcerer Soup hurls a fiery ladle at you, inflicting heavy damage to your health.",
-                        "A mystical soup with potent magical properties."),
-                new Enemy("Cursed Carrot", new StatBlock( 5, 0, 2, 3), "Medium",
-                        "Toxic Root Slam- The Cursed Carrot slams its toxic roots into the ground, weakening your defenses.",
-                        "A carrot cursed by dark magic, capable of unleashing havoc."),
-                new Enemy("Haunted Hamburger", new StatBlock(7, 0, 3, 4), "Hard",
-                        "Seductive Sizzle- The Haunted Hamburger emits a seductive sizzle, enchanting you and lowering your charisma.",
-                        "A haunted hamburger possessed by vengeful spirits."));
-    }
-
-    public boolean startGame(){
-        logger.println("""
-                  ____                         _            _   _                    _                 _                  \s
-                 |  _ \\                       | |          | | (_)          /\\      | |               | |                 \s
-                 | |_) | ___  __ _ _ __ ______| |_ __ _ ___| |_ _  ___     /  \\   __| |_   _____ _ __ | |_ _   _ _ __ ___ \s
-                 |  _ < / _ \\/ _` | '_ \\______| __/ _` / __| __| |/ __|   / /\\ \\ / _` \\ \\ / / _ \\ '_ \\| __| | | | '__/ _ \\\s
-                 | |_) |  __/ (_| | | | |     | || (_| \\__ \\ |_| | (__   / ____ \\ (_| |\\ V /  __/ | | | |_| |_| | | |  __/\s
-                 |____/ \\___|\\__,_|_| |_|      \\__\\__,_|___/\\__|_|\\___| /_/    \\_\\__,_| \\_/ \\___|_| |_|\\__|\\__,_|_|  \\___|\
-                
-                """);
-        if (!getStartInput()) {
-            logger.writeln("What a loser!");
-            return false;
-        }
-        Player player = playerManager.createPlayer();
-        ObstacleSystem obstacleSystem = new ObstacleSystem(random, new OldBean(scanner, logger), new FloorCrack(scanner, logger, random));
-        PathManager pathManager = new PathManager(player, enemyManager, itemManager, obstacleSystem, logger, scanner, 6);
-        Obstacle start = new JourneyStart(logger, scanner, new CombatSystem(logger, scanner, player, enemyManager.getEnemy(1), itemManager));
-        Obstacle end = new JourneyEnd(logger);
-        return gameOver(pathManager.generatePath(start, end));
-    }
-
-    private boolean getStartInput() {
-        UserChoice<Boolean> choices = new UserChoice<>(scanner, logger, "Do you want to play the game?",
-                List.of(
-                        new ChoiceOption<>("Yes", () -> true),
-                        new ChoiceOption<>("No", () -> false)
-                ));
-        return choices.getChoice().outcome().get();
-    }
-
-    public boolean gameOver(boolean won){
-        if (won) {
-            logger.writeln("Your are the mightiest Bean there ever was!");
-        } else {
-            logger.writeln("Better luck next time!");
+        public Main(Random random, Logger logger, Scanner scanner, EnemyManager enemyManager, ItemManager itemManager,
+                        PlayerManager playerManager) {
+                this.random = random;
+                this.logger = logger;
+                this.scanner = scanner;
+                this.enemyManager = enemyManager;
+                this.itemManager = itemManager;
+                this.playerManager = playerManager;
         }
 
-        logger.println("""
-                      ____    _    __  __ _____    _____     _______ ____   \s
-                     / ___|  / \\  |  \\/  | ____|  / _ \\ \\   / / ____|  _ \\  \s
-                    | |  _  / _ \\ | |\\/| |  _|   | | | \\ \\ / /|  _| | |_) | \s
-                    | |_| |/ ___ \\| |  | | |___  | |_| |\\ V / | |___|  _ < _\s
-                     \\____/_/   \\_\\_|  |_|_____|  \\___/  \\_/  |_____|_| \\_(_)
-                    """);
+        public static void main(String[] args) throws IOException, InterruptedException {
 
-        UserChoice<Boolean> playAgain = new UserChoice<>(scanner, logger, "Play again?",
-                List.of(
-                        new ChoiceOption<>("Yes!", () -> true),
-                        new ChoiceOption<>("No!", () -> false)
-                ));
-        return playAgain.getChoice().outcome().get();
-    }
+                String accessToken = GitHubOAuth.getAccessToken();
+
+                Random random = new Random();
+                Scanner scanner = new Scanner(System.in);
+                Logger logger = new Logger(System.out);
+
+                ApitController apitController = new ApitController(accessToken);
+
+                List<PlayerClass> playerClasses = apitController.getClasses();
+                PlayerManager playerManager = new PlayerManager(logger, scanner, playerClasses);
+
+                List<Enemy> enemies = apitController.getEnemies();
+                EnemyManager enemyManager = new EnemyManager(random, enemies.toArray(Enemy[]::new));
+                List<ItemClass> items = apitController.getItems();
+                ItemManager itemManager = new ItemManager(logger, random, scanner,
+                                items.toArray(ItemClass[]::new));
+                Main main = new Main(random, logger, scanner, enemyManager, itemManager, playerManager);
+
+                // START GAME
+                boolean play;
+                do {
+                        play = main.startGame();
+                } while (play);
+
+        }
+
+        // TEMP DATA INSERTION\\
+        public static PlayerClassManager addClass() {
+                return new PlayerClassManager(
+                                new PlayerClass("Green Bean Warrior",
+                                                "A sturdy bean skilled in close combat, wielding the might of beans with brute force.",
+                                                new StatBlock(5, 3, 2, 2)),
+                                new PlayerClass("Magic Coffee Bean",
+                                                "A magical bean adept at harnessing the mystical energies of beans to cast powerful spells.",
+                                                new StatBlock(3, 2, 3, 3)),
+                                new PlayerClass("Sneaky Black Bean",
+                                                "A nimble bean with a knack for stealth and deception, striking enemies from the shadows.",
+                                                new StatBlock(5, 3, 2, 2)),
+                                new PlayerClass("Divine Lima Bean",
+                                                "A benevolent bean with divine powers, healing allies and smiting foes with righteous fury.",
+                                                new StatBlock(5, 3, 2, 2)),
+                                new PlayerClass("Arrowhead Pinto Bean",
+                                                "A skilled bean archer, raining down arrows upon enemies with deadly precision.",
+                                                new StatBlock(5, 3, 2, 2)));
+        }
+
+        public static ItemManager addItems(Logger logger, Random random, Scanner scanner) {
+                return new ItemManager(
+                                logger,
+                                random,
+                                scanner,
+                                new ItemClass("SHOE", "HOT PINK HIGH HEEL", new StatBlock(0, 0, 0, 1), "Common"),
+                                new ItemClass("tea cup", "steaming black tea", new StatBlock(0, 0, -1, 0), "Rare"));
+        }
+
+        public static EnemyManager addEnemies(Random random) {
+                return new EnemyManager(random,
+                                new Enemy("Sorcerer Soup", new StatBlock(3, 0, 1, 2), "Easy",
+                                                "Fiery Ladle Strike- The Sorcerer Soup hurls a fiery ladle at you, inflicting heavy damage to your health.",
+                                                "A mystical soup with potent magical properties."),
+                                new Enemy("Cursed Carrot", new StatBlock(5, 0, 2, 3), "Medium",
+                                                "Toxic Root Slam- The Cursed Carrot slams its toxic roots into the ground, weakening your defenses.",
+                                                "A carrot cursed by dark magic, capable of unleashing havoc."),
+                                new Enemy("Haunted Hamburger", new StatBlock(7, 0, 3, 4), "Hard",
+                                                "Seductive Sizzle- The Haunted Hamburger emits a seductive sizzle, enchanting you and lowering your charisma.",
+                                                "A haunted hamburger possessed by vengeful spirits."));
+        }
+
+        public boolean startGame() {
+                logger.println("""
+                                  ____                         _            _   _                    _                 _                  \s
+                                 |  _ \\                       | |          | | (_)          /\\      | |               | |                 \s
+                                 | |_) | ___  __ _ _ __ ______| |_ __ _ ___| |_ _  ___     /  \\   __| |_   _____ _ __ | |_ _   _ _ __ ___ \s
+                                 |  _ < / _ \\/ _` | '_ \\______| __/ _` / __| __| |/ __|   / /\\ \\ / _` \\ \\ / / _ \\ '_ \\| __| | | | '__/ _ \\\s
+                                 | |_) |  __/ (_| | | | |     | || (_| \\__ \\ |_| | (__   / ____ \\ (_| |\\ V /  __/ | | | |_| |_| | | |  __/\s
+                                 |____/ \\___|\\__,_|_| |_|      \\__\\__,_|___/\\__|_|\\___| /_/    \\_\\__,_| \\_/ \\___|_| |_|\\__|\\__,_|_|  \\___|\
+
+                                """);
+                if (!getStartInput()) {
+                        logger.writeln("What a loser!");
+                        return false;
+                }
+                Player player = playerManager.createPlayer();
+                ObstacleSystem obstacleSystem = new ObstacleSystem(random, new OldBean(scanner, logger),
+                                new FloorCrack(scanner, logger, random));
+                PathManager pathManager = new PathManager(player, enemyManager, itemManager, obstacleSystem, logger,
+                                scanner, 6);
+                Obstacle start = new JourneyStart(logger, scanner,
+                                new CombatSystem(logger, scanner, player, enemyManager.getEnemy(1), itemManager));
+                Obstacle end = new JourneyEnd(logger);
+                return gameOver(pathManager.generatePath(start, end));
+        }
+
+        private boolean getStartInput() {
+                UserChoice<Boolean> choices = new UserChoice<>(scanner, logger, "Do you want to play the game?",
+                                List.of(
+                                                new ChoiceOption<>("Yes", () -> true),
+                                                new ChoiceOption<>("No", () -> false)));
+                return choices.getChoice().outcome().get();
+        }
+
+        public boolean gameOver(boolean won) {
+                if (won) {
+                        logger.writeln("Your are the mightiest Bean there ever was!");
+                } else {
+                        logger.writeln("Better luck next time!");
+                }
+
+                logger.println("""
+                                  ____    _    __  __ _____    _____     _______ ____   \s
+                                 / ___|  / \\  |  \\/  | ____|  / _ \\ \\   / / ____|  _ \\  \s
+                                | |  _  / _ \\ | |\\/| |  _|   | | | \\ \\ / /|  _| | |_) | \s
+                                | |_| |/ ___ \\| |  | | |___  | |_| |\\ V / | |___|  _ < _\s
+                                 \\____/_/   \\_\\_|  |_|_____|  \\___/  \\_/  |_____|_| \\_(_)
+                                """);
+
+                UserChoice<Boolean> playAgain = new UserChoice<>(scanner, logger, "Play again?",
+                                List.of(
+                                                new ChoiceOption<>("Yes!", () -> true),
+                                                new ChoiceOption<>("No!", () -> false)));
+                return playAgain.getChoice().outcome().get();
+        }
 }
---
+++ src/main/java/com/beantastic/oauth/GitHubOAuth.java
@@ -0,0 +1,114 @@
+package com.beantastic.oauth;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class GitHubOAuth {
+
+    public static String getAccessToken() throws IOException, InterruptedException {
+
+        String clientId = ""; // TODO
+        String clientSecret = "";// TODO
+
+        String authorizationEndpoint = "https://github.com/login/oauth/authorize";
+        String tokenEndpoint = "https://github.com/login/oauth/access_token";
+
+        String redirectUri = "http://localhost:8080";
+
+        String authorizationUrl;
+
+        authorizationUrl = String.format("%s?client_id=%s&redirect_uri=%s", authorizationEndpoint,
+                URLEncoder.encode(clientId, "UTF-8"), URLEncoder.encode(redirectUri, "UTF-8"));
+
+        System.out.println("Please open the following URL in your browser and authorize the application:");
+        System.out.println(authorizationUrl);
+
+        SimpleHttpServer server = new SimpleHttpServer(8080);
+        server.start();
+
+        System.out.println("Waiting for authorization...");
+        String authorizationCode = server.waitForAuthorization();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(tokenEndpoint))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(
+                        String.format("client_id=%s&client_secret=%s&code=%s", clientId, clientSecret,
+                                URLEncoder.encode(authorizationCode, "UTF-8"))))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+        String accessToken = "";
+        if (statusCode == 200) {
+            String responseBody = response.body();
+            accessToken = responseBody.split("&")[0].split("=")[1];
+        } else {
+            System.out.println("Failed to retrieve access token. Status code: " + statusCode);
+        }
+
+        server.stopServer();
+
+        return accessToken;
+    }
+}
+
+class SimpleHttpServer extends Thread {
+    private final int port;
+    private boolean running = true;
+    private String authorizationCode;
+
+    public SimpleHttpServer(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            java.net.ServerSocket serverSocket = new java.net.ServerSocket(port);
+            while (running) {
+                java.net.Socket socket = serverSocket.accept();
+                Scanner scanner = new Scanner(socket.getInputStream());
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.contains("GET /?code=")) {
+                        authorizationCode = line.split("GET /\\?code=")[1].split(" ")[0];
+                    }
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                }
+                String response = "HTTP/1.1 200 OK\r\n\r\n<html><body><h1>Authorization successful! You can close this window now.</h1></body></html>";
+                socket.getOutputStream().write(response.getBytes("UTF-8"));
+                socket.close();
+                running = false;
+                scanner.close();
+            }
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String waitForAuthorization() {
+        while (authorizationCode == null) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return authorizationCode;
+    }
+
+    public void stopServer() {
+        running = false;
+    }
+}
